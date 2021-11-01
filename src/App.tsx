dd3664f2b47543dd149424a64b298beaa9b7712c
Nathally Souza <nathallyccd@gmail.com>
2021-11-01 17:08:34 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import {
+  ChakraProvider,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react"
 
 function App() {
+
+  interface IPlayer {
+    player_id: string,
+    players_name: string,
+    games: number,
+    goals: number,
+    assists: number,
+  }
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('http://localhost:5000/players')
+      setData(await response.json())
+    }
+
+    getData()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Heading size="xs">
+        Soccer project
+      </Heading>
+      <Table variant="striped" colorScheme="teal">
+        <TableCaption>
+          <a href="https://github.com/Soccer-Project" target="_blank" rel="noreferrer">
+            Soccer Project on GitHub
+          </a>
+        </TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Player</Th>
+            <Th>Games</Th>
+            <Th isNumeric>Goals</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.map( (player: IPlayer) => {
+            return(
+              <Tr key={player.player_id}>
+                <Td>{player.players_name}</Td>
+                <Td>{player.games}</Td>
+                <Td isNumeric>{player.goals}</Td>
+              </Tr>
+            )
+          })}
+        </Tbody>
+      </Table>
+    </ChakraProvider>
   );
 }
 
