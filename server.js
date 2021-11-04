3370d31582d159a5f64d7ecaf965f0b3b223b9a6
Nathally Souza <nathally.souza@thoughtworks.com>
2021-11-04 20:39:41 -0300

    Fix server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.static)
+app.use(express.static('build'))
 
 app.listen( PORT , () => {
     console.log(`Listening on port ${PORT}`)
