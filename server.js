cab0524634fdfd355250779267ec8fcf0329e4a7
Nathally Souza <nathally.souza@thoughtworks.com>
2021-11-04 20:25:53 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static)
+
+app.listen( PORT , () => {
+    console.log(`Listening on port ${PORT}`)
+})
