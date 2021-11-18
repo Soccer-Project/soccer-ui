ee275920940125b4fb53e958d15ddb9c2ed5bd70
Nathally Souza <72207361+nathyts@users.noreply.github.com>
2021-11-18 12:02:52 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <ChakraProvider>
       <BrowserRouter>
         <Heading size="xs">
-          Soccer project
+          Soccer
         </Heading>
         <MainRoutes />
       </BrowserRouter>
